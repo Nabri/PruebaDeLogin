7bb56e2d91528eff8916fb13633d401b61bcd7fa
unknown <brian.burgos@hotmail.com>
2021-06-21 20:26:04 -0300

    Se crea el proyecto con el registro y sesion funcionando

---
+++ Context/PruebaDeLoginDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaDeLogin.Context
+{
+    public class PruebaDeLoginDbContext : IdentityDbContext
+    {
+        public PruebaDeLoginDbContext()
+        {
+        }
+
+        public PruebaDeLoginDbContext(DbContextOptions<PruebaDeLoginDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
